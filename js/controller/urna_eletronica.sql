66818f8e6871573552d914487a81802f2aba5d8c
niely30 <116914898+niely30@users.noreply.github.com>
2023-01-28 23:44:06 -0300

    Update urna_eletronica.sql

--- js/controller/urna_eletronica.sql
+++ js/controller/urna_eletronica.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 12-Jan-2023 às 08:19
+-- Tempo de geração: 28-Jan-2023 às 18:38
 -- Versão do servidor: 10.4.27-MariaDB
 -- versão do PHP: 8.2.0
 
@@ -33,6 +33,36 @@ CREATE TABLE `candidatos` (
   `nome` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
+--
+-- Extraindo dados da tabela `candidatos`
+--
+
+INSERT INTO `candidatos` (`id`, `numero`, `nome`) VALUES
+(1, 0, 'BRANCO'),
+(2, 13, 'LULA'),
+(3, 22, 'BOLSONARO');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `usuarios`
+--
+
+CREATE TABLE `usuarios` (
+  `id` int(12) NOT NULL,
+  `usuario` varchar(20) NOT NULL,
+  `senha` varchar(255) NOT NULL,
+  `tituloEleitor` varchar(12) NOT NULL,
+  `nomeCompleto` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- Extraindo dados da tabela `usuarios`
+--
+
+INSERT INTO `usuarios` (`id`, `usuario`, `senha`, `tituloEleitor`, `nomeCompleto`) VALUES
+(9, 'Niely', '$2y$10$9pSSx0AqGJ6tRYY5ZIbw0.2PmK6Uk2TThlg50rLHxcZQGJ5gS/VAG', '123456789123', 'Niely Patricia');
+
 -- --------------------------------------------------------
 
 --
@@ -46,6 +76,24 @@ CREATE TABLE `votacao` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 --
+-- Extraindo dados da tabela `votacao`
+--
+
+INSERT INTO `votacao` (`id`, `num_titulo`, `id_candidato`) VALUES
+(1, 2147483647, 2),
+(2, 2147483647, 3),
+(3, 2147483647, 3),
+(4, 2147483647, 2),
+(5, 111111111, 3),
+(6, 1111111, 2),
+(7, 2147483647, 3),
+(8, 2147483647, 2),
+(9, 99999999, 3),
+(10, 2147483647, 1),
+(11, 2147483647, 2),
+(12, 2147483647, 2);
+
+--
 -- Índices para tabelas despejadas
 --
 
@@ -56,11 +104,17 @@ ALTER TABLE `candidatos`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Índices para tabela `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Índices para tabela `votacao`
 --
 ALTER TABLE `votacao`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `id_candidato` (`id_candidato`);
+  ADD KEY `fk_candidato` (`id_candidato`) USING BTREE;
 
 --
 -- AUTO_INCREMENT de tabelas despejadas
@@ -70,13 +124,19 @@ ALTER TABLE `votacao`
 -- AUTO_INCREMENT de tabela `candidatos`
 --
 ALTER TABLE `candidatos`
-  MODIFY `id` int(2) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(2) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT de tabela `usuarios`
+--
+ALTER TABLE `usuarios`
+  MODIFY `id` int(12) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT de tabela `votacao`
 --
 ALTER TABLE `votacao`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- Restrições para despejos de tabelas
@@ -92,3 +152,4 @@ COMMIT;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+
